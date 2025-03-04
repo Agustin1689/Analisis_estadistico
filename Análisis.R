7511a87d07e21962537cf31162f02da895aee31d
Agustín Gagliardi <agustin.gagliardi.castro@gmail.com>
2025-03-04 15:30:05 -0300

    lectura de librerías y datos

---
+++ Análisis.R
@@ -0,0 +1,12 @@
+#Previamente debe tener las librerías instaladas
+
+# Cargar librerías
+library(titanic)
+library(ggplot2)
+library(dplyr)
+library(psych)
+
+# Cargar el dataset
+data <- titanic::titanic_train
+
+
